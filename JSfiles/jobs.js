5de314e6545d78ae4e8109b5f37922f601875e0e
sylvan-vrieze <109949757+sylvan-vrieze@users.noreply.github.com>
2022-08-10 13:06:34 +0200

    Add files via upload

--- JSfiles/jobs.js
+++ JSfiles/jobs.js
@@ -1,4 +1,4 @@
-import { food,wood,stone,copperOre,coal,copperIngot,knowledge } from "./resources.js"
+import { resources } from "./resources.js"
 function job(id,name,active,max,prodType,prodAmount,compType,compAmount,uipresent) {
     this.id = id;
     this.name = name;
@@ -14,12 +14,12 @@ function job(id,name,active,max,prodType,prodAmount,compType,compAmount,uipresen
     }
     this.uipresent = uipresent; 
 }
-const lumberjack = new job(0,"lumberjack",[0,1],0,[wood],[0.1],["none"],[0],false);
-const qaurryworker = new job(1,"qaurryworker",[0,1],0,[stone],[0.1],["none"],[0],false);
-const miner = new job(2,"miner",[0,1],0,[copperOre],[0.1],["none"],[0],false); 
-const coalminer = new job(3,"coalminer",[0,1],0,[coal],[0.1],["none"],[0],false);
-const smelter = new job(4,"smelter",[0,1],0,[copperIngot],[0.01],[copperOre,coal],[0.2,0.3],false); 
-const farmer = new job(5,"farmer",[0,1],0,[food],[0.2],["none"],[0],false); 
-const librarian = new job(6,"librarian",[0,1],0,[knowledge],[0.02],["none"],[0],false);
-//const = new job(,"",[0,1],0,[""],,[""],[],false)
+const lumberjack = new job(0,"lumberjack",0,0,[resources.wood],[0.1],["none"],[0],false);
+const qaurryworker = new job(1,"qaurryworker",0,0,[resources.stone],[0.1],["none"],[0],false);
+const miner = new job(2,"miner",0,0,[resources.copperOre],[0.1],["none"],[0],false); 
+const coalminer = new job(3,"coalminer",0,0,[resources.coal],[0.1],["none"],[0],false);
+const smelter = new job(4,"smelter",0,0,[resources.copperIngot],[0.01],[resources.copperOre,resources.coal],[0.2,0.3],false); 
+const farmer = new job(5,"farmer",0,0,[resources.food],[0.2],["none"],[0],false); 
+const librarian = new job(6,"librarian",0,0,[resources.knowledge],[0.02],["none"],[0],false);
+//const = new job(,"",0,0,[""],,[""],[],false)
 export { lumberjack,qaurryworker,miner,coalminer,smelter,farmer,librarian }
