2c33ce0f1831c65bf98c262a281050e7e4ef481c
sylvan-vrieze <109949757+sylvan-vrieze@users.noreply.github.com>
2022-09-06 14:22:25 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const func = {
     getId: (i) => document.getElementById(i),
+    removeElement: (id) => func.getId(id).remove(),
     checkCost: (resType,resAmount) => {
         for(var i = 0; i < resType.length; i++) {
             if(resType[i].amount < resAmount[i]) {
@@ -7,7 +8,7 @@ const func = {
             } 
         }
         for(var c = 0; c < resType.length; c++) {
-            resType[c].amount -= resAmount[c] ;   
+            resType[c].amount -= resAmount[c];   
         }
         return 1;
     },
@@ -17,18 +18,134 @@ const func = {
         "*": function(a,b) { return a * b},
         ":": function(a,b) { return a / b},
     },
+    create: {
+        button: (item) => {
+            const button = document.createElement("div");
+            button.id = item.name;
+            if(item.constructor.name == "building") {
+                button.innerHTML = `
+                <div class="buildingBuy" id="${item.name}Buy">
+                    <span class="buildingText">${item.name}</span>
+                </div>
+                <div class="buildingCount" id="${item.name}Count">0</div>
+                <div class="buildingSell"  id="${item.name}Sell">sell</div>`;
+                func.getId(`${item.type} ${item.constructor.name}`).appendChild(button);
+                func.getId(item.name).classList.add("buildingButton");
+                func.getId(`${item.name}Sell`).addEventListener("click",function() {buildings.func.changeAmount(item,'-')});
+                func.getId(`${item.name}Buy`).addEventListener("click",function() {buildings.func.changeAmount(item,'+')});
+            } else {
+                button.onclick = function() { item.effect(); }
+                button.innerHTML = item.name
+                func.getId(`${item.constructor.name} content`).appendChild(button);
+                func.getId(item.name).classList.add("button");
+            }
+            button.onmouseover = function() {func.tooltip.edit(`${item.constructor.name}`,item)}; 
+            
+        },
+        tab: (tabName) => {
+            const tab = document.createElement("button");
+            tab.id = tabName;
+            tab.innerHTML = tabName;
+            tab.onclick = function() {openTab(event, `${tabName} content`)};
+            func.getId("tab").appendChild(tab);
+            func.getId(tabName).classList.add("tablinks") ;
+            
+            const tabcontent = document.createElement("div");
+            tabcontent.id = `${tabName} content`;
+            func.getId("contentlist").appendChild(tabcontent);
+            func.getId(tabcontent.id).classList.add("tabcontent");
+        }
+    },
+    tooltip: {
+        edit: (type,item) => {
+            if(type == "gather") {
+                var text = `gather 1 ${item.name}`
+            } else if(type == "building") {
+                var text = `${item.name} <br>`;
+                text += func.tooltip.cost(item.cost.resource,item.cost.amount);
+                if(item.job.type != "none") {
+                    for(var i = 0; i < item.job.type.length ; i++) {
+                        if(i == 0) {
+                            text += `provides ${item.job.amount[i]} ${item.job.type[i].name} job<br>`;
+                        } else {
+                            text += `and ${item.job.amount[i]} ${item.job.type[i].name} job<br>`;
+                        }
+                    }
+                }
+                var part = ["storage","modifier","storage","production"]
+                for(var i = 0; i < 2; i++) {
+                    if(item[part[i]].type != "none" && item[part[i]].type[0].unlocked == true) {
+                        text += `increases ${part[i+2]} for `;
+                        for(var j = 0; j < item[part[i]].type.length; j++) {
+                            if(i == 0) {
+                                text += `${item[part[i]].type[j].name} by ${item[part[i]].amount[j]}<br>`;
+                            } else {
+                                text += `${item[part[i]].type[j].name} by ${item[part[i]].amount[j]*100}%<br>`
+                            }
+                        }
+                    }
+                }
+            } else if(type == "job") {
+                var text = `${item.name} <br>`;
+                var part = ["production","comsumption","produces","comsumes"]
+                for(var i = 0; i < 2; i++) {
+                    if(item[part[i]].type[0] != "none"){
+                        text += part[i+2];
+                        for(var j = 0; j < item[part[i]].type.length; j++) {
+                            text += ` ${item[part[i]].amount[j]*10} ${item[part[i]].type[j].name} per second<br>`;
+                            if(item[part[i]].type.length > 1 && (j+1) < item[part[i]].type.length) {
+                                text += "and"
+                            }
+                        }
+                    }
+                }
+            } else if(type == "upgrade") {
+                var text = `${item.name}<br>
+                ${func.tooltip.cost(item.cost.type,item.cost.amount)}
+                ${item.explanation}</br>`;
+                if(item.modifier.type[0] != "none") {
+                    for(var i = 0; i < item.modifier.type.length; i++) {
+                        text += `increases the production of ${item.modifier.type[i].name} by ${item.modifier.amount[i]*100}%<br>`
+                    }
+                }
+            } else if(type == "research") {
+                var text = `${item.name} <br> ${func.tooltip.cost(item.cost.type,item.cost.amount)} ${item.explanation}`;
+            } else if(type.includes("market")) {
+                var text = `${item.name}<br>`;
+                var amount = func.getId("marketInput").value;
+                if(type.includes("buy")) {
+                    text += `buy ${amount} <br> cost: ${(Math.round((item.cost.current*amount)*1000)/1000)} gold`;
+                } else {
+                    text += `sell ${amount} <br> gain: ${(Math.round(((item.cost.current*amount)*0.8)*1000)/1000)} gold`;
+                }
+            } 
+            func.getId('tooltip').innerHTML = text;
+        },
+        cost: (costType,costAmount) => {
+            var text = "cost: ";
+            for(var i = 0; i < costType.length; i++) {
+                text += `${costAmount[i]} ${costType[i].name} `;
+                if(costType.length > 1 && (i+1) < costType.length) {
+                    text += "and "
+                }
+            }
+            text += "<br>";
+            return text;
+        },
+    },
 }
 export { func }
 //-----------------------------------------------------------------------------------------------------------------------------------------
 import { resources } from "./JSfiles/resources.js"
 import { jobs } from "./JSfiles/jobs.js"
-import { buildings,building } from "./JSfiles/buildings.js"; 
+import { buildings } from "./JSfiles/buildings.js"; 
+import { upgrades } from "./JSfiles/upgrade.js";
 //----------------------------------------------------------------------------------------------------------------------------------
 const market = {
     unlocked: false,
     amount: 0,
     create: () => {
-        addtab("market");
+        func.create.tab("market");
         var input = document.createElement("input");
         input.type = "number";
         input.id = "marketInput";
@@ -51,24 +168,37 @@ const market = {
         func.getId("market content").appendChild(curDiv);
         func.getId(`${curres.name}buy`).onclick = function() {market.buy(curres)};
         func.getId(`${curres.name}sell`).onclick = function() {market.sell(curres)};
-        func.getId(`${curres.name}buy`).onmouseover = function() {editTooltip('market buy',curres)};
-        func.getId(`${curres.name}sell`).onmouseover = function() {editTooltip('market sell',curres)};
+        func.getId(`${curres.name}buy`).onmouseover = function() {func.tooltip.edit('market buy',curres)};
+        func.getId(`${curres.name}sell`).onmouseover = function() {func.tooltip.edit('market sell',curres)};
         func.getId(`${curres.name}buy`).classList.add("marketButton");
         func.getId(`${curres.name}sell`).classList.add("marketButton");
     },
     buy: (curres) => {
         market.amount = Number(func.getId("marketInput").value)
-        if(resources.gold.amount >= (curres.cost*market.amount)) {
+        if(resources.gold.amount >= (curres.cost.current*market.amount)) {
             curres.amount += market.amount;
-            resources.gold.amount -= (curres.cost*market.amount);
+            resources.gold.amount -= (curres.cost.current*market.amount);
+            market.changePrice(curres,"+") 
         }
     },
     sell: (curres) => {
         market.amount = Number(func.getId("marketInput").value)
         if(curres.amount >= market.amount) {
             curres.amount -= market.amount;
-            resources.gold.amount += ((curres.cost*market.amount)*0.8);
-        } 
+            resources.gold.amount += ((curres.cost.current*market.amount)*0.8);
+            market.changePrice(curres,"-") 
+        }
+    },
+    changePrice: (curres,op) => {
+        var changeMultipler = Math.round((Math.random() * 2)*10)/10
+        switch (op) {
+            case "+":
+                curres.cost.current = curres.cost.current * (1 + ((market.amount / 50000) * changeMultipler));
+                func.tooltip.edit('market buy',curres); break;
+            case "-":
+                curres.cost.current = curres.cost.current / (1 + ((market.amount / 50000) * changeMultipler));
+                func.tooltip.edit('market sell',curres); break;
+        }
     },
 }
 export { market }
@@ -117,7 +247,8 @@ function addProdAndComp(id,prodres,prodAmount,compres,compAmount) {
         jobType.production.amount.push(prodAmount);
         prodres.production += (prodAmount * jobType.active);
         updateProd(prodres) 
-    } else if(compres != "none") {
+    }
+    if(compres != "none") {
         jobType.comsumption.type.push(compres);
         jobType.comsumption.amount.push(compAmount);
         compres.comsumption += (compAmount * jobType.active);
@@ -125,80 +256,6 @@ function addProdAndComp(id,prodres,prodAmount,compres,compAmount) {
     }
 }
 export { changeModifier,addProdAndComp,changeProdAndComp }
-//---------------------------------------------------------------------------------------------------------------------------------
-function editTooltip(type,item) {
-    if(type == "gather") {
-        var text = `gather 1 ${item.name}`;
-    } else if(type == "building") {
-        var text = `${item.name} <br>`;
-        text += costEdit(item.cost.resource,item.cost.cost);
-        if(item.job.type != "none") {
-            text += `provides ${item.job.amount} ${item.job.type[0].name} job<br>`;
-        }
-        if(item.storage.type != "none") {
-            text += `increases storage for `;
-            for(var s = 0; s < item.storage.type.length ;s++) {
-                var curres = item.storage.type[s];
-                if(curres.unlocked == true) {
-                    text += `${item.storage.type[s].name} by ${item.storage.amount[s]}<br>`;
-                }
-            }
-        }
-        if(item.modifier.type != "none") {
-            text += `increases production for `;
-            for(var i = 0; i < item.modifier.type.length ;i++) {
-                text += `${item.modifier.type[i].name} by ${item.modifier.multiplier[i]*100}%<br>`;
-            }
-        }
-    } else if(type == "job") {
-        var text = `${item.name} <br>`;
-        if(item.production.type != "none"){
-            text += `produces `;
-            for(var i = 0; i < item.production.type.length ;i++) {
-                text += `${item.production.amount[i]*10} ${item.production.type[i].name} per second<br>`;
-            }
-        }
-        if(item.comsumption.type != "none"){
-            text += `comsumes `;
-            for(var i = 0; i < item.comsumption.type.length ;i++) {
-                text += `${item.comsumption.amount[i]*10} ${item.comsumption.type[i].name} per second<br>`;
-            }
-        }
-    } else if(type == "upgrade") {
-        var text = `${item.name}<br>
-        ${costEdit(item.cost.type,item.cost.amount)}
-        ${item.explanation}</br>`;
-        if(item.modifier.type[0] != "none") {
-            for(var i = 0; i < item.modifier.type.length; i++) {
-                text += `increases the production of ${item.modifier.type[i].name} by ${item.modifier.amount[i]*100}%<br>`
-            }
-        }
-    } else if(type == "research") {
-        var text = `${item.name} <br> ${costEdit(item.cost.type,item.cost.amount)} ${item.explanation}`;
-    } else if(type.includes("market")) {
-        var text = `${item.name}<br>`;
-        var amount = func.getId("marketInput").value;
-        if(type.includes("buy")) {
-            text += `buy ${amount} <br> cost: ${(Math.round((item.cost*amount)*100)/100)} gold`;
-        } else {
-            text += `sell ${amount} <br> gain: ${(Math.round(((item.cost*amount)*0.8)*100)/100)} gold`;
-        }
-    } 
-    func.getId('tooltip').innerHTML = text;
-}
-function costEdit(costType,costAmount) {
-    var text = "cost: ";
-    if(costType.length > 1) {
-        for(var i = 0; i < costType.length; i++) {
-            text += `${costAmount[i]} ${costType[i].name} `;
-        } 
-    } else {
-        text += `${costAmount} ${costType[0].name}`;
-    }
-    text += "<br>";
-    return text;
-}
-export { editTooltip }
 //---------------------------------------------------------------------------------------------------------------------------------
 function autoProduction() {
     for (const entry of Object.entries(resources)) {
@@ -262,8 +319,22 @@ function populationCheck() {
     }
     updateProd(resources.food);
 }
+function pricechange() {
+    for (const entry of Object.entries(resources)) {
+        var curRes = entry[1]
+        if(entry[0] != "func") {
+            var changeMultipler = Math.round((Math.random() * 2)*10)/10
+            if(curRes.cost.current > curRes.cost.start) {
+                curRes.cost.current = curRes.cost.current / (1 + (changeMultipler / 10))
+            } else if (curRes.cost.current < curRes.cost.start) {
+                curRes.cost.current = curRes.cost.current * (1 + (changeMultipler / 10))
+            }
+        }
+    }
+}
 setInterval(function(){autoProduction();},100);
 setInterval(function(){populationCheck();},10000);
+setInterval(function(){pricechange();},60000)
 //----------------------------------------------------------------------------------------------------------------------------------
 window.cheatTest = function(op) {
     for(const entry of Object.entries(resources)) {
@@ -291,50 +362,9 @@ function openTab(evt,tabName) {
     func.getId(tabName).style.display = "block";
     evt.currentTarget.className += " active";
 }
-//-----------------------------------------------------------------------------------------------------------------------------------
-function removeElement(id) { func.getId(id).remove(); };
-function addtab(tabName) {
-    const tab = document.createElement("button");
-    tab.id = tabName;
-    tab.innerHTML = tabName;
-    tab.onclick = function() {openTab(event, `${tabName} content`)};
-    func.getId("tab").appendChild(tab);
-    func.getId(tabName).classList.add("tablinks") ;
-    
-    const tabcontent = document.createElement("div");
-    tabcontent.id = `${tabName} content`;
-    func.getId("contentlist").appendChild(tabcontent);
-    func.getId(tabcontent.id).classList.add("tabcontent");
-}
-function createButton(item) {
-    const button = document.createElement("div");
-    button.id = item.name;
-    if(item.constructor == building) {
-        button.innerHTML = `
-        <div class="buildingBuy" id="${item.name}Buy">
-            <span class="buildingText">${item.name}</span>
-        </div>
-        <div class="buildingCount" id="${item.name}Count">0</div>
-        <div class="buildingSell"  id="${item.name}Sell">sell</div>`;
-        func.getId(`${item.type} ${item.constructor.name}`).appendChild(button);
-        func.getId(item.name).classList.add("buildingButton");
-        func.getId(`${item.name}Sell`).addEventListener("click",function() {changeBuildingAmount(item,'-')});
-        func.getId(`${item.name}Buy`).addEventListener("click",function() {changeBuildingAmount(item,'+')});
-    } else {
-        button.onclick = function() {
-            var effect = item.effect;
-            effect();
-        }
-        button.innerHTML = item.name
-        func.getId(`${item.constructor.name} content`).appendChild(button);
-        func.getId(item.name).classList.add("button");
-    }
-    button.onmouseover = function() {editTooltip(`${item.constructor.name}`,item)}; 
-    
-}
-export { removeElement,addtab,createButton }
 //-----------------------------------------------------------------------------------------------------------------------------------------
 func.getId("buildingTabButton").addEventListener("click",function(){ openTab(event,'building content')});
+func.getId("warfareButton").addEventListener("click",function(){ upgrades.func.createWarvareTab() });
 for(var i = 0; i < 5 ; i++) {
     if(i < 3) {
         addOnclickForClickResouces(i)
@@ -344,12 +374,12 @@ for(var i = 0; i < 5 ; i++) {
 function addOnclickForClickResouces(i) {
     var curRes = Object.entries(resources)[i][1]
     func.getId(`${curRes.name}Gather`).addEventListener("click",function() {resources.func.click(curRes)});
-    func.getId(`${curRes.name}Gather`).addEventListener("mouseover",function() {editTooltip("gather",curRes)});
+    func.getId(`${curRes.name}Gather`).addEventListener("mouseover",function() {func.tooltip.edit("gather",curRes)});
 }
 function addOnclickForBuilding(i) {
     var curBuilding = Object.entries(buildings)[i][1]
-    func.getId(`${curBuilding.name}`).addEventListener("mouseover",function(){editTooltip("building",curBuilding)})
-    func.getId(`${curBuilding.name}Buy`).addEventListener("click",function(){buildings.func.changeBuildingAmount(curBuilding,"+")})
-    func.getId(`${curBuilding.name}Sell`).addEventListener("click",function(){buildings.func.changeBuildingAmount(curBuilding,"-")})
+    func.getId(`${curBuilding.name}`).addEventListener("mouseover",function(){func.tooltip.edit("building",curBuilding)})
+    func.getId(`${curBuilding.name}Buy`).addEventListener("click",function(){buildings.func.changeAmount(curBuilding,"+")})
+    func.getId(`${curBuilding.name}Sell`).addEventListener("click",function(){buildings.func.changeAmount(curBuilding,"-")})
 }
 //------------------------------------------------------------------------------------------------------------------------------------------
