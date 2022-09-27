7614e7a4dc4a522daa1752bd1c94414841213e41
sylvan-vrieze <109949757+sylvan-vrieze@users.noreply.github.com>
2022-09-27 15:14:26 +0200

    Add files via upload

--- JSfiles/warfare.js
+++ JSfiles/warfare.js
@@ -1,10 +1,14 @@
 import { resources } from "./resources.js";
 import { func } from "../main.js";
-function nation(id,name,selling,buying) {
+function nation(id,name,selling,buying,UTamount,units) {
     this.id = id
     this.name = name
     this.selling = selling
     this.buying = buying
+    this.army = {
+        amount:UTamount, 
+        units:units,
+    }
 }
 function unit(id,name,CUamount,Mamount,COamount,resourceCost) {
     this.id = id
@@ -20,9 +24,9 @@ function unit(id,name,CUamount,Mamount,COamount,resourceCost) {
 }
 const warfare = {
     nations: {
-        nation1: new nation(0,"nation1",[resources.food,resources.wood,resources.stone],[resources.coal]),
-        nation2: new nation(1,"nation2",[resources.coal],[resources.food,resources.copperOre]),
-        nation3: new nation(2,"nation3",[resources.copperIngot,resources.ironIngot],[resources.fur]),
+        nation1: new nation(0,"nation1",[resources.food,resources.wood,resources.stone],[resources.coal],[],[]),
+        nation2: new nation(1,"nation2",[resources.coal],[resources.food,resources.copperOre],[],[]),
+        nation3: new nation(2,"nation3",[resources.copperIngot,resources.ironIngot],[resources.fur],[],[]),
     },
     units : {
         warrior: new unit(0,"warrior",0,10,[25],[resources.copperIngot]),
@@ -49,18 +53,26 @@ const warfare = {
             nation.classList.add("nationCard")
             warfare.func.createNationButtons(curNation)
         },
-        createNationButtons: (curNation) => {
-            const diplomacy = document.createElement("div")
-            diplomacy.id = `${curNation.name}diplomacy`
-            diplomacy.innerHTML = "diplomacy"
-            func.getId(`${curNation.name}`).appendChild(diplomacy)
-            func.addClass(`${curNation.name}diplomacy`,"button")
+        createNationButtons: (curNation) => { 
+            for(let i = 0; i < 2; i++) {
+                let type = ["diplomacy","attack"]
+                const button = document.createElement("div") 
+                button.id = `${curNation.name}${type[i]}`
+                button.innerHTML = `${type[i]}`
+                func.getId(`${curNation.name}`).appendChild(button)
+                func.addClass(`${curNation.name}${type[i]}`,"button")
+                func.onclick(`${curNation.name}${type[i]}`,warfare.func[type[i]])
+            }
+        },
+        diplomacy: () => {
 
-            const attack = document.createElement("div")
-            attack.id = `${curNation.name}attack`
-            attack.innerHTML = "attack"
-            func.getId(`${curNation.name}`).appendChild(attack)
-            func.addClass(`${curNation.name}attack`,"button")
+        },
+        attack: () => {
+            for(const unit of Object.entries(warfare.units)) {
+                if(unit[1].amount.current == 0) {
+                    
+                }
+            }
         },
         createUnitUI: (curUnit) => {
             const unit = document.createElement("div")
@@ -70,7 +82,7 @@ const warfare = {
             func.addClass(`${curUnit.name}`,"unitButton")
             func.addClass(`${curUnit.name}Text`,"unitText")
             func.addClass(`${curUnit.name}Amount`,"unitAmount")
-            func.getId(`${curUnit.name}`).addEventListener("click",function(){warfare.func.trainUnit(curUnit)})
+            func.getId(`${curUnit.name}`).addEventListener("click",() => warfare.func.trainUnit(curUnit))
         },
         trainUnit: (unit) => {
             if(unit.amount.current == unit.amount.max) {
